c1a13530b1f4d42d007b48fe8158abec9e945f4b
jakekasan <jake.kasan@gmail.com>
2018-01-25 16:05:21 +0000

    added new development branch

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
-var express = require("express");
-var events = require("events");
-var routing = require("./controllers/routing.js")
+new Vue{{
+  el: '#vue-app',
+  data: {
+    last-updated:'today',
+    author:'jake',
+    meals:[
+      {
+        name: 'Sushi',
+        picture: 01,
+        ingredients:[ 'rice', 'seaweed', 'salmon', 'avocado' ]
+      },
+      {
+        name: 'Steak and Peppercorn Sauce'
+        picture: 02,
+        ingredients:[ 'beef steak', 'beef stock', 'quark', 'rice']
+      },
+      {
+        name: 'Proscuitto e Funghi Pizza'
+        picture: 03,
+        ingredients: ['pizza dough', 'tomato sauce', 'ham','mushrooms']
+      }
+    ]
+  },
+  methods: {
 
-var app = express();
-
-app.listen(9999);
+  },
+  computed: {
+    
+  }
+}};
