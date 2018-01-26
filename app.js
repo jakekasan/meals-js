5845875734428ed3f9a0b25d984ed3f60898c9dc
jakekasan <jake.kasan@gmail.com>
2018-01-26 14:35:56 +0000

    fixed cards

--- app.js
+++ app.js
@@ -17,6 +17,26 @@ new Vue({
         name: 'Proscuitto e Funghi Pizza',
         picture: '03',
         ingredients: ['pizza dough', 'tomato sauce', 'ham','mushrooms']
+      },
+      {
+        name: 'Svickova',
+        picture: '04',
+        ingredients: ['beef sirloin','dumplings','cream and vegetable sauce']
+      },
+      {
+        name: 'Spagetti Bolognese',
+        picture: '05',
+        ingredients: ['beef mince', 'tomato sauce','pasta']
+      },
+      {
+        name: 'Egg pancake',
+        picture: '06',
+        ingredients: ['eggs','mushrooms','cheddar']
+      },
+      {
+        name: 'Roast chicken',
+        pictures: '07',
+        ingredients: ['chicken whole','onion','carrot','celery','garlic']
       }
     ]
   }
