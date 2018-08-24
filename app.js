1351be4f06f204b85e8473316dbbd785e93839d3
jakekasan <jake.kasan@gmail.com>
2018-08-24 15:28:00 +0100

    added ejs and materialize files

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app = express();
 
+const routes = require("./controllers/routes.js");
+
+app.set("view engine","ejs");
+
+app.use(express.static("public"));
+
+routes(app);
+
 app.listen(8000,() => {
     console.log("Meals up and running.\nListening at 8000");
-})
+});
