0fa800c9ed7466cb3a763890c3b2a387c4acb966
jakekasan <jake.kasan@gmail.com>
2018-08-24 14:10:44 +0100

    restarted project

--- README.md
+++
@@ -1,7 +0,0 @@
-# Meals-JS
-_To-Do_:
-
-- [ ] Create bindings with CouchDB
-  - [ ] Allow for user input to create, modify and delete elements in that database
-- [ ] Create dynamic cards in main page
-- [ ] Create sidebar navigation
--- app.js
+++ app.js
@@ -1,43 +1,6 @@
-new Vue({
-  el:"#vue-app",
-  data:{
-    name:'jake',
-    meals:[
-      {
-        name: 'Sushi',
-        picture: '01',
-        ingredients:[ 'rice', 'seaweed', 'salmon', 'avocado' ]
-      },
-      {
-        name: 'Steak and Peppercorn Sauce',
-        picture: '01',
-        ingredients:[ 'beef steak', 'beef stock', 'quark', 'rice']
-      },
-      {
-        name: 'Proscuitto e Funghi Pizza',
-        picture: '03',
-        ingredients: ['pizza dough', 'tomato sauce', 'ham','mushrooms']
-      },
-      {
-        name: 'Svickova',
-        picture: '04',
-        ingredients: ['beef sirloin','dumplings','cream and vegetable sauce']
-      },
-      {
-        name: 'Spagetti Bolognese',
-        picture: '05',
-        ingredients: ['beef mince', 'tomato sauce','pasta']
-      },
-      {
-        name: 'Egg pancake',
-        picture: '06',
-        ingredients: ['eggs','mushrooms','cheddar']
-      },
-      {
-        name: 'Roast chicken',
-        pictures: '07',
-        ingredients: ['chicken whole','onion','carrot','celery','garlic']
-      }
-    ]
-  }
-});
+const express = require("express");
+const app = express();
+
+app.listen(8000,() => {
+    console.log("Meals up and running.\nListening at 8000");
+})
