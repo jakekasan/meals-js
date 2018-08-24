4b13ad275806335c91034c873becf319e5f48350
jakekasan <jake.kasan@gmail.com>
2018-08-24 16:12:07 +0100

    added temporary fake data

---
+++ controllers/data.js
@@ -0,0 +1,217 @@
+recipes = [
+    {
+        name:"Scrambled Eggs",
+        ingredients:[
+            {
+                name:"Egg",
+                quantity:200
+            },
+            {
+                name:"Butter",
+                quantity:50
+            },
+            {
+                name:"Cheese",
+                quantity:50
+            }
+        ]
+    },
+    {
+        name:"Bolognese",
+        ingredients:[
+            {
+                name:"Beef Mince",
+                quantity:500
+            },
+            {
+                name:"Chopped Tomatoes",
+                quantity:400
+            },
+            {
+                name:"Onion",
+                quantity:100
+            },
+            {
+                name:"Mushroom",
+                quantity:100
+            },
+            {
+                name:"Garlic",
+                quantity:50
+            },
+            {
+                name:"Pasta",
+                quantity:200
+            }
+        ]
+    },
+    {
+        name:"Chili",
+        ingredients:[
+            {
+                name:"Beef Mince",
+                quantity:500
+            },
+            {
+                name:"Chopped Tomatoes",
+                quantity:400
+            },
+            {
+                name:"Peppers",
+                quantity:100
+            },
+            {
+                name:"Onion",
+                quantity:100
+            },
+            {
+                name:"Chili",
+                quantity:50
+            }
+        ]
+    },
+    {
+        name:"Chicken Fajitas",
+        ingredients:[
+            {
+                name:"Chicken Breast",
+                quantity:500
+            },
+            {
+                name:"Peppers",
+                quantity:100
+            },
+            {
+                name:"Onion",
+                quantity:200
+            },
+            {
+                name:"Cheese",
+                quantity:100
+            },
+            {
+                name:"Wrap",
+                quantity:100
+            }
+        ]
+    },
+    {
+        name:"Chicken Carbonara",
+        ingredients:[
+            {
+                name:"Chicken Breast",
+                quantity:500
+            },
+            {
+                name:"Onion",
+                quantity:100
+            },
+            {
+                name:"Cheese",
+                quantity:100
+            },
+            {
+                name:"Mushroom",
+                quantity:100
+            },
+            {
+                name:"Pasta",
+                quantity:200
+            }
+        ]
+    },
+    {
+        name:"Roast Chicken and Potatoes",
+        ingredients:[
+            {
+                name:"Chicken Breast",
+                quantity:300
+            },
+            {
+                name:"Onions",
+                quantity:100
+            },
+            {
+                name:"Potato",
+                quantity:500
+            }
+        ]
+    },
+    {
+        name:"Cottage Pie",
+        ingredients:[
+            {
+                name:"Beef Mince",
+                quantity:500
+            },
+            {
+                name:"Potato",
+                quantity:500
+            },
+            {
+                name:"Carrot",
+                quantity:100
+            },
+            {
+                name:"Onion",
+                quantity:100
+            }
+        ]
+    }
+]
+
+ingredients = [
+    {
+        name:"Beef Mince",
+        quantity:750
+    },
+    {
+        name:"Chicken Breast",
+        quantity:600
+    },
+    {
+        name:"Egg",
+        quantity:400
+    },
+    {
+        name:"Onion",
+        quantity:80
+    },
+    {
+        name:"Carrot",
+        quantity:60
+    },
+    {
+        name:"Potato",
+        quantity:1000
+    },
+    {
+        name:"Pasta",
+        quantity:500
+    },
+    {
+        name:"Mushroom",
+        quantity:220
+    },
+    {
+        name:"Cheese",
+        quantity:300
+    },
+    {
+        name:"Wrap",
+        quantity:400
+    },
+    {
+        name:"Chili",
+        quantity:80
+    },
+    {
+        name:"Chopped Tomatoes",
+        quantity:440
+    }
+]
+
+module.exports = {
+    recipes,
+    ingredients
+}
