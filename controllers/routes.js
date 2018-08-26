dd08c568c7b107102bc13eb44ce0aa654f4cc6f9
jakekasan <jake.kasan@gmail.com>
2018-08-26 14:46:35 +0100

    main page now dynamically loads recipes

--- controllers/routes.js
+++ controllers/routes.js
@@ -1,30 +1,108 @@
-const data = require("./data");
+const database = require("./data");
 const fetch = require("node-fetch");
 
 module.exports = (app,address) => {
     app.get("/",(req,res) => {
-        res.render("home");
+        let url = new URL(address+"/api/data");
+        url.search = new URLSearchParams(req.query);
+        fetch(url)
+            .then(result => result.json())
+            .then(result => {
+                res.render("home",{data:result});
+            });
     });
 
     // data API
 
-    app.get("/api/data/recipe",(req,res) => {
+    app.get("/api/data",(req,res) => {
+        console.log("/api/data");
         console.log(req.query);
-        if (req.query.q){
+        let dow = ["monday","tuesday","wednesday","thrusday","friday","saturday","sunday"];
+        let filledDays = req.query;
+        let required = dow.filter(item => {
+            return !(Object.keys(filledDays)).includes(item)
+        });
+
+        console.log("Filled days:");
+        console.log(filledDays);
+
+        let promises = [];
+
+        for (let day of required){
+            console.log("Filling",day);
+            let includedRecipes = (Object.keys(req.query)).map(key => { return req.query[key] });
+            let available = (database.recipes.filter(item => !includedRecipes.includes(item.name))).map(item => item.name);
+            let chosenRecipe = available.pop(Math.floor(Math.random() * available.length));
+            console.log(chosenRecipe);
+            includedRecipes.push(chosenRecipe);
+            filledDays[day] = chosenRecipe;
+            let url = new URL(address+"/api/data/recipe");
+            url.search = new URLSearchParams({
+                name:chosenRecipe
+            })
+            let promise = fetch(url)
+                                .then(data => {
+                                    data.json();
+                                })
+                                .then(data => {
+                                    console.log("Data for",day);
+                                    console.log(data);
+                                    return {
+                                        day:day,
+                                        data:data
+                                    }
+                                });
+            promises.push(promise);
+        }
+
+        Promise.all(promises)
+                .then(values => {
+                    console.log("Promises resolved");
+                    console.log(values);
+                    for (let value in values){
+                        filledDays[value.day] = value.data;
+                    }
+
+                    res.send(filledDays);
+                })
+
+
+    });
+
+    app.get("/api/data/recipe",(req,res) => {
+        console.log("/api/data/recipe");
+        console.log(req.query);
+
+        if (req.query.random){
+            // say we want a completely random recipe
+            let result = database.recipes[Math.floor(Math.random()*database.recipes.length)];
+            console.log("/api/data/recipe random");
+            result.ingredients = result.ingredients.map(item => JSON.stringify(item));
+            let url = new URL(address+"/api/data/recipe/fill");
+            url.search = new URLSearchParams(result);
+            fetch(url)
+                .then(data => data.json())
+                .then(data => {
+                    res.send(data);
+                });
+        } else if (req.query.q){
             // search for results to recipe
             // still to do...
             console.log("Got req.query.q")
         } else if (req.query.name){
             // search for direct name, return first match
-            let result = data.recipes.filter(item => {
-                console.log(typeof item.name);
-                console.log(typeof req.query.name);
+            let result = database.recipes.filter(item => {
                 return ((item.name) == (req.query.name))
+            }).pop();
 
-            });
-            console.log(result);
-            console.log(JSON.stringify(result))
-            res.send(JSON.stringify(result));
+            let url = new URL(address+"/api/data/recipe/fill");
+
+            url.search = new URLSearchParams(result);
+            fetch(url)
+                .then(data => data.json())
+                .then(data => {
+                    res.send(data);
+                });
         } else {
             console.log("Sending empty string");
             res.send(JSON.stringify([]));
@@ -33,8 +111,13 @@ module.exports = (app,address) => {
 
     app.get("/api/data/recipe/fill", (req,res) => {
 
-        let recipe = data.recipes.filter(item => (item.name == req.query.name)).pop();
+        console.log("/api/data/recipe/fill");
+        console.log(req.query);
+        //console.log(database.ingredients);
 
+        let recipe = database.recipes.filter(item => (item.name == req.query.name)).pop();
+
+        console.log("/api/data/recipe/fill recipe");
         console.log(recipe);
 
         let ingredientsPromises = recipe.ingredients.map(item => {
@@ -43,7 +126,7 @@ module.exports = (app,address) => {
                 name:item.name,
                 quantity:item.quantity
             });
-            return fetch(url).then(data => data.json())
+            return fetch(url).then(data => JSON.stringify(data.json()))
         });
 
         Promise.all(ingredientsPromises)
@@ -69,7 +152,7 @@ module.exports = (app,address) => {
             quantity:((req.query.quantity) ? req.query.quantity : undefined)
         };
 
-        let ingredient = data.ingredients.filter(item => (item.name == query.name)).pop();
+        let ingredient = database.ingredients.filter(item => (item.name == query.name)).pop();
 
         if (!ingredient){
             res.send(JSON.stringify([]));
