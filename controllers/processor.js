4b2a5676abc6ba804f75c682c2678eb5233c05cb
jakekasan <jake.kasan@gmail.com>
2018-08-29 16:10:35 +0100

    new set of functions to handle filling requests

---
+++ controllers/processor.js
@@ -0,0 +1,48 @@
+/*
+
+    processor.js
+
+    - Here is where requests are processed
+
+    ideal user session:
+
+    {
+        cookie: // user session cookie
+        mealPlan: {
+            monday: // recipe
+        }
+        groceries: [
+            // list of groceries
+        ]
+    }
+
+*/
+
+const Imports = require("./imports");
+
+function main(req,res,userSession){
+    if (!userSession){
+        // no userSession given, get one
+
+        Imports.Cookies.getUserSession(req,res);
+        return
+    }
+
+    if (!userSession.mealPlan){
+        Imports.Recipes.getMealPlan(req,res,userSession);
+        return
+    }
+
+    if (!userSession.groceries){
+        Imports.Groceries.getGroceries(req,res,userSession);
+        return
+    }
+
+    // maybe some validation?
+
+    // then respond
+
+    res.cookie = userSession.cookie;
+    res.render("home",{data:userSession});
+}
+
