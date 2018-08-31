993547aaa01a799825c556d0f30abe59051e997f
jakekasan <jake.kasan@gmail.com>
2018-08-31 11:13:40 +0100

    new versions of recipe getters

--- controllers/database/recipes.js
+++ controllers/database/recipes.js
@@ -6,15 +6,50 @@
 
 */
 
-function getMealPlan(req,res,userSession){
-    userSession.mealplan = Object.keys(userSession.mealPlan).map(key => {
-        let data = userSession.mealPlan[key];
-        if (data){
-            return fillMealDay(data)
-        } else {
-            return data
-        }
-    })
+const Processor = require("../processor");
+const Imports = require("../imports");
+const fakeData = require("./fake_data");
+
+const Mongoose = require("mongoose");
+
+function getAllRecipes(res){
+    res.json(fakeData.recipes);
 }
 
-module.exports = "Nothing yet...";
+function getRecipeByName(name,res){
+    return fakeData.recipes
+        .filter(item => (item.name == name))
+        .pop();
+}
+
+
+
+// function getMealPlan(req,res,userSession){
+//     userSession.mealPlan
+//     let mealPlanPromises = Object.keys(userSession.mealPlan).map(key => {
+//         let nameOfRecipe = userSession.mealPlan[key];
+//         if (nameOfRecipe){
+//             return fillMealDay(nameOfRecipe)
+//         } else {
+//             return nameOfRecipe
+//         }
+//     });
+
+//     Processor.main(req,res,userSession);
+// }
+
+// function fillMealDay(data){
+//     // check if data is not null
+//     if (!data){
+//         return
+//     }
+
+//     fetch("https://localhost:8080/api/data/recipe")
+
+
+// }
+
+module.exports = {
+    getAllRecipes,
+    getRecipeByName
+};
