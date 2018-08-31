820e5a4f08d253d5101807810487a27f9708bf52
jakekasan <jake.kasan@gmail.com>
2018-08-31 14:11:14 +0100

    added processor to imports

--- controllers/imports.js
+++ controllers/imports.js
@@ -11,6 +11,7 @@ const Cookies = require("./database/cookies");
 const Recipes = require("./database/recipes");
 const Data = require("./database/fake_data");
 const Ingredients = require("./database/ingredients");
+const Processor = require("./processor");
 
 
 module.exports = {
