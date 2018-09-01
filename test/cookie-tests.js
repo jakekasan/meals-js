0b7a7684b652ee7777890ead92376e79a6d4c84d
jakekasan <jake.kasan@gmail.com>
2018-09-02 00:16:54 +0100

    added second test for cookieMiddlewere

--- test/cookie-tests.js
+++ test/cookie-tests.js
@@ -1,6 +1,7 @@
 const Cookies = require("./../controllers/imports").Cookies;
 
 function test_noCookie(){
+    console.log("Testing noCookie...")
     let test_req = {
         cookies:{}
     }
@@ -20,10 +21,13 @@ function test_noCookie(){
 }
 
 function test_cookieMiddleware(){
+    console.log("Testing cookieMiddleware...")
     let test_req = {
+        name:"test_req",
         cookies:{}
     }
     let test_res = {
+        name:"test_res",
         cookie: function(key,value){
             this[key] = value;
             console.log(`test_res object : ${key} set to ${value}`)
@@ -34,6 +38,29 @@ function test_cookieMiddleware(){
         console.log(test_req);
         console.log(test_res);
     }
+
+    Cookies.cookieMiddleware(test_req,test_res,test_next);
+
+    // now test an existing cookie
+
+    test_req = {
+        name:"test_req_withCookie",
+        cookies:{
+            _id:0
+        }
+    }
+
+    test_res = {
+        name:"test_res",
+        cookie: function(key,value){
+            this[key] = value;
+            console.log(`test_res object : ${key} set to ${value}`)
+        }
+    }
+
+    Cookies.cookieMiddleware(test_req,test_res,test_next);
 }
 
 test_noCookie();
+
+test_cookieMiddleware();
