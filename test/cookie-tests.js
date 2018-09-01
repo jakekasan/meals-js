fcd77887c054897d542d920922ca945c5e3e5462
jakekasan <jake.kasan@gmail.com>
2018-09-01 22:33:36 +0100

    added test for middleware function

--- test/cookie-tests.js
+++ test/cookie-tests.js
@@ -19,4 +19,21 @@ function test_noCookie(){
     Cookies.noCookie(test_req,test_res,test_next);
 }
 
+function test_cookieMiddleware(){
+    let test_req = {
+        cookies:{}
+    }
+    let test_res = {
+        cookie: function(key,value){
+            this[key] = value;
+            console.log(`test_res object : ${key} set to ${value}`)
+        }
+    }
+    let test_next = function(){
+        console.log("Next function called");
+        console.log(test_req);
+        console.log(test_res);
+    }
+}
+
 test_noCookie();
