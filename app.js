01dd695eafa9592755378a258ec94b271e1412ab
jakekasan <jake.kasan@gmail.com>
2018-09-06 14:53:33 +0100

    added urlencoded from bodyParser for form processing

--- app.js
+++ app.js
@@ -16,6 +16,9 @@ app.set("view engine","ejs");
 
 app.use(express.static("public"));
 
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(bodyParser.json());
 
 app.use(Imports.Cookies.cookieMiddleware);
