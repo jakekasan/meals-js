a6402fd4cf722bd8a4384e9f65bd939d9d1c43b5
jakekasan <jake.kasan@gmail.com>
2018-09-06 16:46:40 +0100

    started reworking file to deal with jobs - might turn into middleware

--- controllers/processor.js
+++ controllers/processor.js
@@ -2,50 +2,101 @@
 
     processor.js
 
-    - Here is where requests are processed
-
-    ideal user session:
-
-    {
-        cookie: // user session cookie
-        mealPlan: {
-            monday: // recipe
-        }
-        groceries: [
-            // list of groceries
-        ]
-    }
+    - this keeps track of database jobs
+    - confirmation codes will be handled here
 
 */
 
 const Imports = require("./imports");
+const sqlite3 = require("sqlite3");
 
-function main(req,res,userSession){
-    if (!userSession){
-        // no userSession given, get one
-
-        Imports.Cookies.getUserSession(req,res);
-        return
+let db = sqlite3.Database("./database/sql/databaseJobs.db",(err) => {
+    if (err) {
+        throw err
     }
+    // connected!
+});
 
-    if (!userSession.mealPlan){
-        Imports.Recipes.getMealPlan(req,res,userSession);
-        return
-    }
+function initDatabase(){
+    // run just once
+    let db = sqlite3.Database("./database/sql/databaseJobs.db",(err) => {
+        if (err) {
+            throw err
+        }
+        // connected!
+    });
 
-    if (!userSession.groceries){
-        Imports.Groceries.getGroceries(req,res,userSession);
-        return
-    }
+    
 
-    // maybe some validation?
-
-    // then respond
-
-    res.cookie = userSession.cookie;
-    res.render("home",{data:userSession});
+    db.close();
 }
 
-module.exports = {
-    main
+function addJob(req,res,jobType,jobName){
+
+    // connect to the database
+
+    let db = sqlite3.Database("./database/sql/databaseJobs.db",(err) => {
+        if (err) {
+            throw err
+        }
+        // connected!
+        
+        // if the table doesnt exist, create it
+
+        db.run(`CREATE TABLE jobs(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            type TEXT,
+            name TEXT,
+            done INTEGER
+        ) IF NOT EXISTS (SELECT * FROM jobs)`,(err) => {
+            if (err) throw err;
+
+            // now insert the job
+            let sql = `INSERT INTO jobs (type, name, done) VALUES (?,?,?)`;
+
+            let values = [jobType,jobName,0];
+
+            db.run(sql,values,(err) => {
+                if (err) throw err;
+                let jobID = this.lastID;
+                res.redirect(`/${jobType}?confirmation=${jobID}`);
+                db.close();
+            });
+
+        });
+    });
+}
+
+function checkJob(req,res,jobID){
+    let db = sqlite3.Database("./database/sql/databaseJobs.db",(err) => {
+        if (err) {
+            throw err
+        }
+        // connected!
+        
+        // if the table doesnt exist, create it
+
+        db.run(`CREATE TABLE jobs(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            type TEXT,
+            name TEXT,
+            done INTEGER
+        ) IF NOT EXISTS (SELECT * FROM jobs)`,(err) => {
+            if (err) throw err;
+
+            // now insert the job
+            let sql = `SELECT (type,name,done) FROM jobs WHERE id = ?`;
+
+            let values = [jobID];
+
+            db.run(sql,values,(err,row) => {
+                if (err) throw err;
+                if (row.done == 0){
+                    // on second thought, i'll turn this into middleware
+                    res.render();
+                }
+            });
+
+        });
+    });
 }
