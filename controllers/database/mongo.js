1aa83810fe7db965dc48cc79430523ec33e9e7d3
jakekasan <jake.kasan@gmail.com>
2018-09-07 13:03:45 +0100

    added callback for addRecipe

--- controllers/database/mongo.js
+++ controllers/database/mongo.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schemas = require("./schemas");
+const Imports = require("./../imports");
 
 mongoose.connect("mongodb://<test>:<password1>@ds245512.mlab.com:45512/meals");
 
@@ -10,12 +11,25 @@ mongoose.Promise = global.Promise;
 const RecipeModel = mongoose.model("Recipe",Schemas.Recipe);
 const IngredientsModel = mongoose.model("Ingredient",Schemas.Ingredient);
 
-function addRecipe(recipe){
+function addRecipe(req,res){
     // check recipe
-    let newRecipe = new RecipeModel(recipe);
-    newRecipe.save((err, savedRecipe) => {
-        if (err) return console.error(err);
-    })
+
+    let recipe = req.body;
+
+    function addRecipeCallback(jobID){
+
+        let newRecipe = new RecipeModel(recipe);
+        newRecipe.save((err, savedRecipe) => {
+            if (err) {
+                // model failed to save, ammend job entry
+                Imports.Processor.jobFailure(jobID);
+            }
+            // model saved, ammend job entry
+            Imports.Processor.jobSuccess(jobID);
+        });
+    }
+
+    Imports.Processor.addJob(req,res,addRecipeCallback);
 }
 
 function addIngredient(ingredient){
