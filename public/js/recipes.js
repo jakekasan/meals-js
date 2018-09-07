526e6a9ed8fee915bd4e21313762b1b167e04423
jakekasan <jake.kasan@gmail.com>
2018-09-07 21:14:18 +0100

    addGroceries button is now disabled if the ingredient name is not valid

--- public/js/recipes.js
+++ public/js/recipes.js
@@ -5,10 +5,46 @@ window.addEventListener("load",() => {
         M.textareaAutoResize(element);
     }
 
-    // ingredientsList
     
+    // ingredients
+
+    fetch("http://localhost:8000/api/recipes")
+        .then(data => data.json())
+        .then(data => {
+            // initialize the materialize element
+            let ingredientNameInput = document.getElementById("ingredientName");
+            let instance = M.Autocomplete.init([ingredientNameInput],{
+                data:data.reduce((acc,elem) => {
+                    acc[elem.name] = null;
+                    return acc
+                },{})
+            });
+
+            let submitIngredient = document.getElementById("addIngredient");
+            const submitIngredientClass = submitIngredient.className;
+            
+            ingredientNameInput.addEventListener("keyup",() => {
+                // if the value is equal to any of the ingredients in the list, remove the disabled value from the form button
+                let value = ingredientNameInput.value;
+
+                if (!(data.map(item => item.name)).includes(value)){
+                    submitIngredient.className = submitIngredientClass + " disabled";
+                } else {
+                    submitIngredient.className = submitIngredientClass;
+                }
+            });
+
+        })
+
     
 
+    // on keyup of the name typing 
+
+    let ingredientNameInput = document.getElementById("ingredientName");
+    ingredientNameInput.addEventListener("keyup",() => {
+
+    })
+    
     let addIngredientButton = document.getElementById("addIngredient");
 
     addIngredientButton.addEventListener("click",() => {
