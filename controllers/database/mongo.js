343a9ac940a4ed51ffefb1e8c4135d0343002cd0
jakekasan <jake.kasan@gmail.com>
2018-09-07 18:41:01 +0100

    fixed handling of requests

--- controllers/database/mongo.js
+++ controllers/database/mongo.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
 const Schemas = require("./schemas");
-const Imports = require("./../imports");
+const Imports = require("../imports");
+const Processor = require("./../processor");
 
-mongoose.connect("mongodb://<test>:<password1>@ds245512.mlab.com:45512/meals");
+mongoose.connect("mongodb://temp:password1@ds245512.mlab.com:45512/meals",{
+    useNewUrlParser:true
+});
 
-const db = mongoose.connection();
+//const db = mongoose.connection();
 
 mongoose.Promise = global.Promise;
 
@@ -16,22 +19,53 @@ function addRecipe(req,res){
 
     let recipe = req.body;
 
-    function addRecipeCallback(jobID){
+    recipe.ingredients = recipe.ingredients.map(item => JSON.parse(item));
 
-        let newRecipe = new RecipeModel(recipe);
-        newRecipe.save((err, savedRecipe) => {
-            if (err) {
-                // model failed to save, ammend job entry
-                Imports.Processor.jobFailure(jobID);
-            }
-            // model saved, ammend job entry
-            Imports.Processor.jobSuccess(jobID);
-        });
-    }
+    let newRecipe = new RecipeModel(recipe);
 
-    Imports.Processor.addJob(req,res,addRecipeCallback);
+    console.log(newRecipe);
+
+    newRecipe.save((err) => {
+        if (err) {
+            // model failed to save, ammend job entry
+            //Processor.jobFailure(jobID);
+            console.log(err);
+            console.log("recipe not saved");
+            res.redirect("/recipes");
+            return
+        }
+        console.log(`recipe saved`);
+        res.redirect("/recipes");
+        return
+    });
+
+    return
+
+    // function addRecipeCallback(jobID){
+
+    //     let newRecipe = new RecipeModel(recipe);
+    //     newRecipe.save((err, savedRecipe) => {
+    //         if (err) {
+    //             // model failed to save, ammend job entry
+    //             Processor.jobFailure(jobID);
+    //         }
+    //         // model saved, ammend job entry
+    //         Processor.jobSuccess(jobID);
+    //     });
+    // }
+
+    // console.log("addRecipe");
+
+    // console.log(Processor);
+
+    // Processor.addJob(req,res,"recipes",addRecipeCallback);
 }
 
 function addIngredient(ingredient){
     // check ingredient
+}
+
+module.exports = {
+    addRecipe,
+    addIngredient
 }
