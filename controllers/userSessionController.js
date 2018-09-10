971156beb640173efce6feed323bd0f54ab6b61c
jakekasan <jake.kasan@gmail.com>
2018-09-10 16:22:14 +0100

    added blank userSessionController

---
+++ controllers/userSessionController.js
@@ -0,0 +1,12 @@
+/*
+
+    userSessionController.js
+
+    TO-DO:
+        - create the middleware
+            - check for cookie in request
+                - if cookie, pull userSession from model
+                - if no cookie, create new userSession
+            -
+
+*/
