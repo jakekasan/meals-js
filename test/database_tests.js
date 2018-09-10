d65587dd199be0030551a16c499893dc54012db0
jakekasan <jake.kasan@gmail.com>
2018-09-10 13:59:12 +0100

    added tests for database

---
+++ test/database_tests.js
@@ -0,0 +1,38 @@
+const fetch = require("node-fetch");
+const Mongo = require("./../controllers/imports").Mongo;
+
+class ResponseObject{
+    constructor(){
+        // nothing
+    }
+
+    json(data){
+        console.log(JSON.stringify(data,null,2));
+    }
+}
+
+
+function viewAllRecipes(){
+
+    let res = new ResponseObject();
+
+    Mongo.getAllRecipes(null,res);
+    // let data = await fetch("http://localhost:8000/api/recipes")
+    //     .then(data => data.json())
+    //     .catch(e => {
+    //         throw e
+    //     })
+
+    // console.log(JSON.stringify(data,null,2));
+}
+
+function viewAllIngredients(){
+
+    let res = new ResponseObject();
+
+    Mongo.getAllIngredients(null,res);
+}
+
+//viewAllRecipes();
+
+viewAllIngredients();
