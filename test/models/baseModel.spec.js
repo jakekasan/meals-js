3934b2b1a55e2032dc5021f1b94f5abffafbe0e8
jakekasan <jake.kasan@gmail.com>
2018-09-12 12:15:53 +0100

    added test for baseModel

---
+++ test/models/baseModel.spec.js
@@ -0,0 +1,10 @@
+const assert = require("chai").assert;
+const expect = require("chai").expect;
+const should = require("chai").should();
+const baseModel = require("./../../models/baseModel");
+
+describe("baseModel",() => {
+    it("should have a name attribute",() => {
+        
+    })
+})
