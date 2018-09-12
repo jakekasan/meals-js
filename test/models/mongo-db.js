900c02054f8f004766ed97b68ea5f51c02880d26
jakekasan <jake.kasan@gmail.com>
2018-09-12 12:15:29 +0100

    added test for mongoDB connection

---
+++ test/models/mongo-db.js
@@ -0,0 +1,14 @@
+const assert = require("chai").assert;
+const expect = require("chai").expect;
+const should = require("chai").should();
+
+const mongoose = require("mongoose");
+const config = require("./../../config/index")("development");
+
+describe("MongoDB Connection",() => {
+    it("MongoDB connection error is null",() => {
+        mongoose.connect(config.mongo,(err,conn) => {
+            err.should.be.equal.to(null);
+        });
+    });    
+})
