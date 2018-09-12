863d2a63e3068c223b3e152ca38f2855ea5934e0
jakekasan <jake.kasan@gmail.com>
2018-09-12 10:56:48 +0100

    filled out API controller for ingredients and recipes

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -5,16 +5,72 @@ const ingredientModel = require("./../models/ingredientModel");
 module.exports = baseController.extends({
     name:"API",
     run: function(req,res,next){
-
+        recipeModel.setMongo(req.mongo);
+        ingredientModel.setMongo(req.mongo);
+        if (!Object.keys(this.routes).includes(req.path)){
+            return res.sendStatus(404);
+        }
+        this.routes[req.path][req.method];
     },
     routes:{
-        "api/ingredients": {
+        "/api/ingredients": {
+            GET: function (req,res,next) {
+                ingredientModel.retrieve((req.query || {}),(err,data) => {
+                    if (err) {
+                        return res.sendStatus(500)
+                    }
+                    return res.json(data);
+                })
+            },
+            POST: function (req,res,next) {
+                // post data to model
+                if (!(req.body || req.body.name)){
+                    return res.sendStatus(400)
+                }
+                ingredientModel.retrieve({name:req.body.name},(err,data) => {
+                    if (err){
+                        return res.sendStatus(500)
+                    }
+                    if (data){
+                        ingredientModel.update(data,req.body,(err) => {
+                            if (err){
+                                return res.sendStatus(400)
+                            }
+                            return res.sendStatus(200)
+                        })
+                    }
+
+                })
+            }
+        },
+        "/api/recipes": {
             get: function (req,res,next) {
-                recipeModel.setMongo(req.mongo);
-                recipeModel.retrieve()
+                recipeModel.retrieve((req.query || {}),(err,data) => {
+                    if (err) {
+                        return res.sendStatus(500)
+                    }
+                    return res.json(data);
+                })
             },
             post: function (req,res,next) {
                 // post data to model
+                if (!(req.body || req.body.name)){
+                    return res.sendStatus(400)
+                }
+                recipeModel.retrieve({name:req.body.name},(err,data) => {
+                    if (err){
+                        return res.sendStatus(500)
+                    }
+                    if (data){
+                        recipeModel.update(data,req.body,(err) => {
+                            if (err){
+                                return res.sendStatus(400)
+                            }
+                            return res.sendStatus(200)
+                        })
+                    }
+
+                })
             }
         }
     }
