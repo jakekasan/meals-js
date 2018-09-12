bfada2f7f49859b0aa3dfdea8e0318de41941da4
jakekasan <jake.kasan@gmail.com>
2018-09-12 13:16:09 +0100

    added extend function to baseController

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -1,7 +1,12 @@
+const _ = require("underscore");
+
 module.exports = {
     name:"Base",
     content: null,
     run: function(){
         return
+    },
+    extend: function(child){
+        return _.extend({},this,child)
     }
 }
