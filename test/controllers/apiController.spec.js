4074f62f5c3b0b048ac07b58b59b111e2af38fca
jakekasan <jake.kasan@gmail.com>
2018-09-12 14:09:47 +0100

    added test for API controller

---
+++ test/controllers/apiController.spec.js
@@ -0,0 +1,85 @@
+const assert = require("chai").assert;
+const expect = require("chai").expect;
+const should = require("chai").should();
+const ApiController = require("./../../controllers/apiController");
+
+describe("Api Controller",() => {
+    it("Should have a name of 'Base",() => {
+        ApiController.should.have.property("name").equal("API");
+    });
+    it("Should have a run property",() => {
+        ApiController.should.have.property("run");
+    });
+    it("Should have run property be a function",() => {
+        ApiController["run"].should.be.a("function");
+    });
+    
+    it("Should have 'routes' property",() => {
+        ApiController.should.have.property("routes");
+    })
+
+    const fakeModel = function(){
+        this.setMongo = function(mongo){
+            this.mongo = mongo;
+        };
+        this.create = function(data,callback){
+            if (data) {
+                return callback(null)
+            }
+            return callback(true)
+        };
+        this.retrieve = function(query,callback){
+            if (query){
+                return callback(null,true);
+            }
+            return callback(true,null);
+        };
+        this.update = function(query,update,callback){
+            if (query && update) {
+                return callback(null);
+            } else {
+                return callback(true);
+            }
+        };
+    }
+
+    const fakeReq = function(query,body,path,method,mongo){
+        this.query = query;
+        this.body = body;
+        this.path = path;
+        this.method = method;
+        this.mongo = mongo;
+    }
+
+    const fakeRes = function(callback){
+        this.callback = callback;
+        this.sendStatus = function(code){
+            this.callback(code);
+        };
+        this.json = function(data){
+            this.callback(data);
+        };
+    }
+
+    describe("API Routes",() => {
+
+        describe("/api/ingredients",() => {
+            it("Should have a route for '/api/ingredients'",() => {
+                ApiController.routes.should.have.property("/api/ingredients");
+            });
+
+            it("should have a GET method for '/api/ingredients' ",() => {
+                ApiController.routes["/api/ingredients"].should.have.property("GET");
+            });
+
+            it("should have a POST method for '/api/ingredients' ",() => {
+                ApiController.routes["/api/ingredients"].should.have.property("POST");
+            });
+
+            it("should return a sendStatus(200) if a valid object is sent",() => {
+                let tempController = ApiController;
+                tempController.recipeModel = new fakeModel();
+            })
+        })
+    })
+});
