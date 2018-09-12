67daef519c0c5e9946fe9ab5ffacaed890964b73
jakekasan <jake.kasan@gmail.com>
2018-09-12 11:35:52 +0100

    started working on test for baseController

---
+++ test/controllers/baseController.spec.js
@@ -0,0 +1,16 @@
+const assert = require("chai").assert;
+const expect = require("chai").expect;
+const should = require("chai").should();
+const BaseController = require("./../../controllers/baseController");
+
+describe("Base Controller",() => {
+    it("Should have a name of 'Base",() => {
+        BaseController.should.have.property("name").equal("Base");
+    });
+    it("Should have a run property",() => {
+        BaseController.should.have.property("run");
+    });
+    it("Should have run property be a function",() => {
+        BaseController["run"].should.be.a("function");
+    });
+})
