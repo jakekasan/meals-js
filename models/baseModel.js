39eab6faf43e2a2b36f1dffe2ec22a4d22c6dfee
jakekasan <jake.kasan@gmail.com>
2018-09-18 08:02:01 +0100

    changed data variable name to res

--- models/baseModel.js
+++ models/baseModel.js
@@ -22,15 +22,15 @@ module.exports.prototype = {
     },
     create: function(data,callback){
         let newObject = this.model(data);
-        newObject.save(data,(err,data) => callbackHandler(err,data,callback));
+        newObject.save(data,(err,res) => callbackHandler(err,res,callback));
     },
     retrieve: function(data,callback){
-        this.model.find((data || {}),(err,data) => callbackHandler(err,data,callback));
+        this.model.find((data || {}),(err,res) => callbackHandler(err,res,callback));
     },
     update: function(data,callback){
-        this.model.update(data,(err,data) => callbackHandler(err,data,callback));
+        this.model.update(data,(err,res) => callbackHandler(err,res,callback));
     },
     delete: function(data,callback){
-        this.model.delete(data,(err,data) => callbackHandler(err,data,callback));
+        this.model.delete(data,(err,res) => callbackHandler(err,res,callback));
     }
 }
