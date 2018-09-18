b85f1772a7b59cf2cf41b9e53d17b95b520ee102
jakekasan <jake.kasan@gmail.com>
2018-09-18 08:01:35 +0100

    added express-session

--- controllers/userSessionController.js
+++ controllers/userSessionController.js
@@ -15,32 +15,63 @@ const userSessionModel = require("./../models/userSessionModel");
 
 module.exports = function(req,res,next){
     userSessionModel.setMongo(req.mongo);
+    // express-session not persisting
+
+    
+
     // check if session exists and has a user
-    if (req.session && req.session.id){
+    console.log("Initial req.session",req.session);
+    if (req.session && req.session.user){
         // check that user is valid
-        userSessionModel.retrieve({id:req.session.id},(err,data) => {
+        userSessionModel.retrieve({_id:req.session.user._id},(err,data) => {
             if (err) {
                 userSessionModel.create({},(err,data) => {
-                    console.log(data.mealPlan);
+                    //console.log(data.mealPlan);
+                    console.log("Error retrieving userSession");
+                    console.log(req.session);
+                    console.log("Now the data...");
+                    console.log(data);
                     req.userSession = data;
-                    req.session.id = data._id;
-                    next();
+                    req.session.user = data;
+                    req.session.save(() => {
+                        console.log(req.session);
+                        return next();
+                    });
+                    return
                 });
             } else {
+                console.log("Retrieved userSession");
+                console.log(req.session);
+                console.log("Now the data...");
+                console.log(data);
                 req.userSession = data;
-                req.session.id = data._id;
-                next();
-                return
+                req.session.user = data;
+                req.session.save(() => {
+                    console.log(req.session);
+                    return next();
+                });
+                
             }
         });
     } else {
         // create a new userSession
         userSessionModel.create({},(err,data) => {
+            console.log("Creating new userSession..");
+            console.log(req.session);
+            console.log("Now the data...");
             console.log(data);
             req.userSession = data;
-            req.session.id = data._id;
-            next();
+            req.session.user = data;
+            req.session.save(() => {
+                if (req.count) {
+                    req.session.count++;
+                } else {
+                    req.session.count = 1;
+                }
+                console.log(req.session);
+                return next();
+            });
+            
         });
-        return
     }
 }
