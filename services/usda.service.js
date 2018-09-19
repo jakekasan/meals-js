20409741d75a221b5770bf669f8568e381b1d4ef
jakekasan <jake.kasan@gmail.com>
2018-09-19 14:01:55 +0100

    added debug and nutrition path

--- services/usda.service.js
+++ services/usda.service.js
@@ -8,6 +8,7 @@ const config = require("./../config/index");
 const fetch = require("node-fetch");
 
 module.exports = {
+    debug:false,
     search: function(req,res,next){
         let query = req.query;
         fetch(config.usda.address.search,{
@@ -22,6 +23,16 @@ module.exports = {
             .catch(e => res.sendStatus(500))
     },
     nutrition: function(req,res,next){
-
+        let query = req.query;
+        fetch(config.usda.address.nutrition,{
+            method:"post",
+            body:{
+                q:query,
+                api_key:config.usda.apiKey
+            }
+        })
+            .then(data => data.json())
+            .then(data => res.json(data))
+            .catch(e => res.sendStatus(500))
     }
 }
