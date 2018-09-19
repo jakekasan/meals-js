70e44e6f7636812b429e37298236af6e0de3ce57
jakekasan <jake.kasan@gmail.com>
2018-09-19 14:08:16 +0100

    added tests for extend

--- test/controllers/baseController.spec.js
+++ test/controllers/baseController.spec.js
@@ -7,10 +7,34 @@ describe("Base Controller",() => {
     it("Should have a name of 'Base",() => {
         BaseController.should.have.property("name").equal("Base");
     });
+
     it("Should have a run property",() => {
         BaseController.should.have.property("run");
     });
-    it("Should have run property be a function",() => {
+
+    it("Run property be a function",() => {
         BaseController["run"].should.be.a("function");
     });
+
+    it("Should have an extend property",() => {
+        BaseController.should.have.property("extend");
+    });
+
+    it("Extend property should be a function",() => {
+        BaseController["extend"].should.be.a("function");
+    });
+
+    it("Extended controller should inherit from base",() => {
+        let bC = require("./../../controllers/base.controller");
+        let bC2 = bC.extend({});
+
+        bC.name.should.equal(bC2.name);
+    });
+
+    it("Extended controller should not be the same as base",() => {
+        let bC = require("./../../controllers/base.controller");
+        let bC2 = bC.extend({});
+
+        bC.should.not.equal(bC2);
+    })
 });
