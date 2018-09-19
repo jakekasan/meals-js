b0b8ee40a103e3f8b562773322342017c87ee82e
jakekasan <jake.kasan@gmail.com>
2018-09-19 14:41:52 +0100

    added extend tests and fake mongoose model

--- test/models/baseModel.spec.js
+++ test/models/baseModel.spec.js
@@ -4,7 +4,94 @@ const should = require("chai").should();
 const baseModel = require("./../../models/base.model");
 
 describe("baseModel",() => {
-    it("should have a name attribute",() => {
-        
+
+    describe("Basic parameters",() => {
+
+        it("module should be a function", () => {
+            let baseModel = (require("./../../models/base.model"));
+
+            baseModel.should.be.a("function");
+        });
+
+        it("running module function should return an object", () => {
+            let baseModel = (require("./../../models/base.model"));
+
+            let bM = new baseModel();
+
+            (bM).should.be.an("object");
+        });
+
+        it("should have a name attribute",() => {
+            let baseModel = new (require("./../../models/base.model"))();
+
+            baseModel.should.have.property("name");
+        });
+
+    });
+
+    describe("Test fake objects and whatnot",() => {
+
+        it("fakeMongoose should have a property 'save'",() => {
+            fakeMongoose.should.have.property("save");
+        });
+
+        it("fakeMongoose.save should be a function",() => {
+            fakeMongoose.save.should.be.a("function");
+        })
     })
-})
+
+    describe("Model functions",() => {
+        
+        it("Should correctly set this.mongo",() => {
+            let mongooseModel = "mongooseModel";
+
+            let model = new (require("./../../models/base.model"))();
+
+            model.setModel(mongooseModel);
+
+            let newMongooseModel = model.getModel();
+
+            newMongooseModel.should.equal(mongooseModel);
+        });
+
+        it("Should have create property",() => {
+            let model = new (require("./../../models/base.model"))();
+
+            model.should.have.property("create");
+        });
+
+        it("Create property should be a function",() => {
+            let model = new (require("./../../models/base.model"))();
+
+            (model.create).should.be.a("function");
+        });
+
+        it("this.model should return an object with a 'save' property",() => {
+            let model = new (require("./../../models/base.model"))();
+
+            
+        })
+    });
+});
+
+
+/*
+
+    Fake objects and whatnot
+
+*/
+
+const fakeMongoose = {
+    save: function(data,callback){
+        return null
+    },
+    find: function(data,callback){
+        return null
+    },
+    update: function(original,updated,callback){
+        return null
+    },
+    delete: function(data,callback){
+        return null
+    }
+}
