19508a5b6d36255d49994ffe04010affee1c956a
jakekasan <jake.kasan@gmail.com>
2018-09-20 13:41:37 +0100

    filled in some tests for DB

--- test/models/nutritionDB.spec.js
+++ test/models/nutritionDB.spec.js
@@ -20,16 +20,27 @@ describe("Nutrition SQLite3 database model",() => {
             })
         });
 
-        it("SQL table should exist in the db file", () => {
+        it("SQL table should exist in the db file", (done) => {
             let db =  new sqlite.Database(config.databases.nutrition,(err) => {
                 if (err) {
                     throw err
                 }
             });
 
-            let sql = `CREATE TABLE IF NOT EXISTS ingredients(`
+            let sql = `CREATE TABLE IF NOT EXISTS ingredients(
+                ndbno INTEGER PRIMARY KEY,
+                name TEXT,
+                calories NUMBER,
+                proteins NUMBER,
+                carbohydrates NUMBER,
+                fats NUMBER
+            )`
 
-            db.run()
+            db.run(sql,(res,err) => {
+                console.log(err);
+                assert(err === null);
+                done();
+            })
         })
     });
 
