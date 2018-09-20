620af634fe7758dfdf949dff422364ef74324073
jakekasan <jake.kasan@gmail.com>
2018-09-20 17:33:48 +0100

    filled in functionality for searching USDA DB and filling in collection

--- public/js/ingredients/add.js
+++ public/js/ingredients/add.js
@@ -20,6 +20,51 @@ document.addEventListener("DOMContentLoaded",() => {
             } else {
                 (M.Autocomplete.getInstance(item)).close();
             }
+            let url = new URL("http://localhost:8000/api/usda/search");
+            url.search = new URLSearchParams({
+                q:item.value
+            });
+            fetch(url)
+                .then(data => data.json())
+                .then(data => {
+                    console.log(data);
+                    var collection = document.querySelector(".collection");
+                    collection.innerHTML = "";
+
+                    for (let ingredient of data){
+                        // add to collection
+                        console.log(ingredient);
+                        let collectionItem = document.createElement("div");
+                        collectionItem.className = "collection-item";
+                        collectionItem.innerText = ingredient.name;
+                        collectionItem.setAttribute("data-ndbno",ingredient.ndbno);
+                        collectionItem.addEventListener("click",() => {
+                            let url = new URL("http://localhost:8000/api/usda/nutrition");
+                            url.search = new URLSearchParams({
+                                ndbno:ingredient.ndbno
+                            });
+                            fetch(url)
+                                .then(data => {
+                                    console.log(data);
+                                    try {
+                                        return data.json();
+                                    } catch(e) {
+                                        return data
+                                    }
+                                })
+                                .then(data => {
+                                    location.reload();
+                                    // console.log(data);
+                                    // setTimeout(() => {
+                                    //     location.reload();
+                                    // },10000);
+                                })
+                                .catch(e => console.log(e));
+                        });
+                        
+                        collection.appendChild(collectionItem);
+                    }
+                })
         })
     });
 
