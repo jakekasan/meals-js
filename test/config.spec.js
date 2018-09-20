1fdb19f040ce3b9dc62f3cc7b96704ac30d77c1c
jakekasan <jake.kasan@gmail.com>
2018-09-20 13:39:34 +0100

    reorganized tests and added one

--- test/config.spec.js
+++ test/config.spec.js
@@ -24,74 +24,97 @@ describe("Config test",() => {
     });
 
     describe("Development environment",() => {
-        it("config should have 'mode' property",() => {
-            let config = (require("./../config/index"))("development");
-            
-            config.should.have.property("mode");
-        });
+        describe("Basics",() => {
+            it("config should have 'mode' property",() => {
+                let config = (require("./../config/index"))("development");
+                
+                config.should.have.property("mode");
+            });
 
-        it("config should have 'mode' property equal to 'development",() => {
-            let config = (require("./../config/index"))("development");
-
-            config["mode"].should.equal("development");
-        });
-
-        it("config should have server property",() => {
-            let config = (require("./../config/index"))("development");
-            config.should.have.property("server");
+            it("config should have 'mode' property equal to 'development",() => {
+                let config = (require("./../config/index"))("development");
+    
+                config["mode"].should.equal("development");
+            });
         })
-
-        it("config server should have property 'hostname'",() => {
-            let config = (require("./../config/index"))("development");
-            config["server"].should.have.property("hostname");
+        
+        describe("Server params",() => {
+            it("config should have server property",() => {
+                let config = (require("./../config/index"))("development");
+                config.should.have.property("server");
+            })
+    
+            it("config server should have property 'hostname'",() => {
+                let config = (require("./../config/index"))("development");
+                config["server"].should.have.property("hostname");
+            });
+    
+            it("config server should have property 'port'",() => {
+                let config = (require("./../config/index"))("development");
+                config["server"].should.have.property("port");
+            });
         });
 
-        it("config server should have property 'port'",() => {
-            let config = (require("./../config/index"))("development");
-            config["server"].should.have.property("port");
+        describe("Database params",() => {
+            it("config should have a 'databases' property",() => {
+                let config = (require("./../config/index"))("development");
+                
+                config.should.have.property("databases");
+            });
+    
+            it("config database should have a 'mongoDB' property",() => {
+                let config = (require("./../config/index"))("development");
+                
+                config["databases"].should.have.property("mongoDB");
+            });
+
+            it("should have a 'nutritionDB' property",() => {
+                let config = (require("./../config/index"))("development");
+                
+                config["databases"].should.have.property("nutrition");
+            });
+    
         });
 
-        it("config should have a 'databases' property",() => {
-            let config = (require("./../config/index"))("development");
+        describe("USDA",() => {
+            it("config should have a 'usda' property",() => {
+                let config = (require("./../config/index"))("development");
+                
+                config.should.have.property("usda");
+            });
+    
+            it("config['usda] should have a 'apiKey' property",() => {
+                let config = (require("./../config/index"))("development");
+                
+                config["usda"].should.have.property("apiKey");
+            });
+    
+            it("config['usda'] should have a 'address' property",() => {
+                let config = (require("./../config/index"))("development");
+                
+                config["usda"].should.have.property("address");
+            });
             
-            config.should.have.property("databases");
-        });
-
-        it("config database should have a 'mongoDB' property",() => {
-            let config = (require("./../config/index"))("development");
+            it("config.usda.address should have a 'search' property",() => {
+                let config = (require("./../config/index"))("development");
+                
+                config.usda.address.should.have.property("search");
+            });
             
-            config["databases"].should.have.property("mongoDB");
-        });
-
-        it("config should have a 'usda' property",() => {
-            let config = (require("./../config/index"))("development");
-            
-            config.should.have.property("usda");
-        });
-
-        it("config['usda] should have a 'apiKey' property",() => {
-            let config = (require("./../config/index"))("development");
-            
-            config["usda"].should.have.property("apiKey");
-        });
-
-        it("config['usda'] should have a 'address' property",() => {
-            let config = (require("./../config/index"))("development");
-            
-            config["usda"].should.have.property("address");
+            it("config.usda.address should have a 'search' property",() => {
+                let config = (require("./../config/index"))("development");
+                
+                config.usda.address.should.have.property("nutrition");
+            });
         });
         
-        it("config.usda.address should have a 'search' property",() => {
-            let config = (require("./../config/index"))("development");
-            
-            config.usda.address.should.have.property("search");
-        });
+
+       
+
+       
         
-        it("config.usda.address should have a 'search' property",() => {
-            let config = (require("./../config/index"))("development");
-            
-            config.usda.address.should.have.property("nutrition");
-        });
+
+
 
     });
     
