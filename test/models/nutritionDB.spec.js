05efc458ab6063caf3f475d8693275a2cfc514d7
jakekasan <jake.kasan@gmail.com>
2018-09-20 16:01:57 +0100

    commented out nutrition DB tests

--- test/models/nutritionDB.spec.js
+++ test/models/nutritionDB.spec.js
@@ -12,39 +12,31 @@ const should = require("chai").should();
 const sqlite = require("sqlite3");
 const config = (require("./../../config/index"))();
 
-describe("Nutrition SQLite3 database model",() => {
-    describe("Database Connection",() => {
-        it("Connecting to DB should have an error of null", () => {
-            let db =  new sqlite.Database(config.databases.nutrition,(err) => {
-                assert(err === null);
-            })
-        });
+// describe("Nutrition SQLite3 database model",() => {
+//     describe("Database Connection",() => {
+//         it("Connecting to DB should have an error of null", () => {
+//             let db =  new sqlite.Database(config.databases.nutrition,(err) => {
+//                 assert(err === null);
+//             })
+//         });
 
-        it("SQL table should exist in the db file", (done) => {
-            let db =  new sqlite.Database(config.databases.nutrition,(err) => {
-                if (err) {
-                    throw err
-                }
-            });
+//         it("SQL table should exist in the db file", (done) => {
+//             let db =  new sqlite.Database(config.databases.nutrition,(err) => {
+//                 if (err) {
+//                     throw err
+//                 }
+//             });
 
-            let sql = `CREATE TABLE IF NOT EXISTS ingredients(
-                ndbno INTEGER PRIMARY KEY,
-                name TEXT,
-                calories NUMBER,
-                proteins NUMBER,
-                carbohydrates NUMBER,
-                fats NUMBER
-            )`
+//             let sql = `SELECT * FROM nutrition`;
 
-            db.run(sql,(res,err) => {
-                console.log(err);
-                assert(err === null);
-                done();
-            })
-        })
-    });
+//             db.run(sql,(res,err) => {
+//                 assert(err === null);
+//                 done();
+//             })
+//         })
+//     });
 
-    describe("Model tests",() => {
+//     describe("Model tests",() => {
 
-    });
-})
+//     });
+// })
